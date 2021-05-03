d65949c859f64248546d2be5e59d21582177e420
Vladislav Garagashev <vladis.garagashev@gmail.com>
2021-05-03 16:13:30 +0500

    fix:Исправить отображение стилей css

--- src/components/App.js
+++ src/components/App.js
@@ -39,56 +39,52 @@ function App() {
   };
 
   return (
-    <div className="page">
-      <div className="page__container">
+    <div className="page__container">
 
-        <Header/>
+      <Header/>
 
-        <Main
+      <Main
           onEditProfile={handleEditProfileClick}
           onEditAvatar={handleEditAvatarClick}
           onAddPlace={handleAddPlaceClick}
           onCardClick={handleCardClick}/>
 
-        <Footer/>
+      <Footer/>
 
-        <PopupWithForm title="Редактировать профиль" name="edit-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-          <section className="form__section">
-            <input className="form__item form__item_element_name" type="text" name="name" id="name" placeholder="Имя" minLength="2" maxLength="40" required/>
-            <span className="form__item-error" id="name-error"></span>
-          </section>
-          <section className="form__section">
-            <input className="form__item form__item_element_job" type="text" name="about" id="about" placeholder="О себе" minLength="2" maxLength="200" required/>
-            <span className="form__item-error" id="about-error"></span>
-          </section>
-        </PopupWithForm>
+      <PopupWithForm title="Редактировать профиль" name="edit-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+        <section className="form__section">
+          <input className="form__item form__item_element_name" type="text" name="name" id="name" placeholder="Имя" minLength="2" maxLength="40" required/>
+          <span className="form__item-error" id="name-error"></span>
+        </section>
+        <section className="form__section">
+          <input className="form__item form__item_element_job" type="text" name="about" id="about" placeholder="О себе" minLength="2" maxLength="200" required/>
+          <span className="form__item-error" id="about-error"></span>
+        </section>
+      </PopupWithForm>
 
-        <PopupWithForm title="Новое место" name="add-card" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
-          <section className="form__section">
-            <input className="form__item form__item_element_name" type="text" name="name" id="image-name" placeholder="Название" minLength="2" maxLength="30" required/>
-            <span className="form__item-error" id="image-name-error"></span>
-          </section>
-          <section className="form__section">
-            <input className="form__item form__item_element_image-link" type="url" name="link" id="image-link" placeholder="Ссылка на картинку" required/>
-            <span className="form__item-error" id="image-link-error"></span>
-          </section>
-        </PopupWithForm>
+      <PopupWithForm title="Новое место" name="add-card" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+        <section className="form__section">
+          <input className="form__item form__item_element_name" type="text" name="name" id="image-name" placeholder="Название" minLength="2" maxLength="30" required/>
+          <span className="form__item-error" id="image-name-error"></span>
+        </section>
+        <section className="form__section">
+          <input className="form__item form__item_element_image-link" type="url" name="link" id="image-link" placeholder="Ссылка на картинку" required/>
+          <span className="form__item-error" id="image-link-error"></span>
+        </section>
+      </PopupWithForm>
 
-        <PopupWithForm title="Обновить аватар" name="edit-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-          <section className="form__section">
-            <input className="form__item form__item_element_image-link" type="url" name="avatar" id="avatar" placeholder="Ссылка на картинку" required/>
-            <span className="form__item-error" id="avatar-error"></span>
-          </section>
-        </PopupWithForm>
+      <PopupWithForm title="Обновить аватар" name="edit-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+        <section className="form__section">
+          <input className="form__item form__item_element_image-link" type="url" name="avatar" id="avatar" placeholder="Ссылка на картинку" required/>
+          <span className="form__item-error" id="avatar-error"></span>
+        </section>
+      </PopupWithForm>
 
-        <PopupWithForm title="Вы уверены?" name="delete-card"/>
+      <PopupWithForm title="Вы уверены?" name="delete-card"/>
 
-        <ImagePopup card={selectedCard} isOpen={isImagePreviePopupOpen} onClose={closeAllPopups}/>
-
-      </div>
+      <ImagePopup card={selectedCard} isOpen={isImagePreviePopupOpen} onClose={closeAllPopups}/>
 
     </div>
-
   );
 };
 
